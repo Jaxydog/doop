cd9fbe2b3e9b122143787dafba40c9dd7e378107
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2023-11-08 09:19:28 -0500

    Skip adding role if it's already held

--- src/cmd/membership/on/modal.rs
+++ src/cmd/membership/on/modal.rs
@@ -166,11 +166,11 @@ pub async fn update<'api: 'evt, 'evt>(
         submission.get_mut().anchor = Some(Anchor::from(message));
     }
 
-    if status == StatusKind::Accepted {
-        member.roles.push(config.get().submission.member_role_id);
-    } else {
-        let member_role_id = config.get().submission.member_role_id;
+    let member_role_id = config.get().submission.member_role_id;
 
+    if status == StatusKind::Accepted && !member.roles.contains(&member_role_id) {
+        member.roles.push(config.get().submission.member_role_id);
+    } else if member.roles.contains(&member_role_id) {
         member.roles.retain(|id| id != &member_role_id);
     }
 
